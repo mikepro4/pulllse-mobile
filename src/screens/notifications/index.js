42c76ec7c90f6f176fb6bc3ab781b4de3f8f03a9
anmst21 <anmstudios21c@gmail.com>
2023-10-26 17:43:02 -0400

    everything works

--- src/screens/notifications/index.js
+++ src/screens/notifications/index.js
@@ -72,30 +72,80 @@ const Notifications = () => {
     }
   };
 
+  const handleSeen = async (notificationId) => {
+    try {
+      // Sending a PUT request to mark the notification as seen
+      await userApi.post("/markNotificationSeen", {
+        notificationId,
+      });
+
+      // Updating the notifications state by setting the 'seen' property of the marked notification
+      setNotifications((prevNotifications) =>
+        prevNotifications.filter(
+          (notification) => notification._id !== notificationId
+        )
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const buttonSwitch = (item) => {
+    switch (item.type) {
+      case "follow":
+        return (
+          <Button
+            title="Mark as seen"
+            onPress={() => handleSeen(item._id)}
+            color="green"
+          />
+        );
+      case "subscription_request":
+        return (
+          <>
+            <Button
+              title="Accept"
+              onPress={() =>
+                handleAccept(storedUserInfo._id, item.from._id, item._id)
+              }
+              color="green"
+            />
+            <Button
+              title="Decline"
+              onPress={() =>
+                handleDecline(storedUserInfo._id, item.from._id, item._id)
+              }
+              color="red"
+            />
+          </>
+        );
+    }
+  };
+
+  const textSwitch = (item) => {
+    switch (item.type) {
+      case "follow":
+        return (
+          <CustomText>{item.from.userName} is now following you</CustomText>
+        );
+      case "subscription_request":
+        return (
+          <CustomText>
+            {item.from.userName} sent you a subscription request
+          </CustomText>
+        );
+    }
+  };
+
   const NotificationItem = ({ item }) => (
     <View style={styles.notificationContainer}>
+      {console.log(item)}
       <Image source={{ uri: item.from.imageLink }} style={styles.userImage} />
-      <View style={styles.textContainer}>
-        <Text>{item.from.userName}</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Accept"
-          onPress={() =>
-            handleAccept(storedUserInfo._id, item.from._id, item._id)
-          }
-          color="green"
-        />
-        <Button
-          title="Decline"
-          onPress={() =>
-            handleDecline(storedUserInfo._id, item.from._id, item._id)
-          }
-          color="red"
-        />
-      </View>
+      <View style={styles.textContainer}>{textSwitch(item)}</View>
+      <View style={styles.buttonContainer}>{buttonSwitch(item)}</View>
     </View>
   );
+
   return (
     <View style={{ backgroundColor: "black", flex: 1, paddingTop: 130 }}>
       <View style={styles.header}>
@@ -113,6 +163,16 @@ const Notifications = () => {
 export default Notifications;
 
 const styles = StyleSheet.create({
+  textContainer: {
+    paddingLeft: 10,
+  },
+  notificationContainer: {
+    marginHorizontal: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
