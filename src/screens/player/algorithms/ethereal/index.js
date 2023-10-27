1a2e05e552eddc1dfd89ef8ecf021bd2d934e2c7
mikepro4 <mikhailpro4@gmail.com>
2023-10-27 05:11:57 -0400

    fucking hell

--- src/screens/player/algorithms/ethereal/index.js
+++ src/screens/player/algorithms/ethereal/index.js
@@ -9,12 +9,13 @@ import {
     Skia,
     SweepGradient,
     useComputedValue,
+    useClockValue,
     useTouchHandler,
     useValue,
     vec,
 } from '@shopify/react-native-skia';
 import { View, Text, StyleSheet, Dimensions, PixelRatio } from 'react-native';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
     Easing,
     cancelAnimation,
@@ -26,42 +27,41 @@ import {
 } from "react-native-reanimated";
 
 const { width, height } = Dimensions.get('window');
+import { useDispatch, useSelector } from "react-redux";
 
 import CustomText from '../../../../components/text';
 
-export const useClock = () => {
-    const clock = useSharedValue(0);
-    useFrameCallback((info) => {
-        clock.value = clock.value + 100
-    });
-    return clock;
-};
+
 
 function Ethereal() {
-    const progress = useValue(0);
-    const clock = useClock();
+    const clock = useClockValue();
+    const player = useSelector((state) => state.player);
+    const [width, setWidth] = useState(null);
+    const [layout, setLayout] = useState({ width: 0, height: 0 });
 
-    // const path = useComputedValue(() => {
-    //     const circles = Skia.Path.Make();
-    
-    //     for (let index = 0; index < 1500; index++) {
- 
-    //       circles.addCircle(x, y, radius);
-    //     }
-    
-    //     return circles;
-    // }, [clock.value]);
+    let activeLayer = player.editedLayers.filter(item => item.position === 1);
 
-    const circles = Skia.Path.Make();
+    const path = useComputedValue(() => {
+        const circles = Skia.Path.Make();
+        // console.log(clock.current / 1000 * -0.01)
+        console.log(activeLayer[0]?.params.boldness)
+        circles.addCircle(clock.current / 100 + 100, activeLayer[0]?.params.boldness * 3 + 100, 1.3);
+        return circles
+    }, [clock, activeLayer])
 
-    circles.addCircle(10, 10, 10);
 
-    let path = circles
+    const handleLayout = (event) => {
+        const { width, height } = event.nativeEvent.layout;
+        setLayout({ width, height });
 
-    // const transform = useDerivedValue(
-    //     () => [{ translateY: 100 }, { rotate: (Math.PI * clock.value) / 4000 }],
-    //     [clock]
-    // );
+        // setDimensions({
+        //     width: width,
+        //     height: height,
+        //     radius: 1,
+        //     x: (width) / 10,
+        //     y: (height) / 10
+        // })
+    }
 
     const renderParam = (name, value) => {
         return (
@@ -76,15 +76,14 @@ function Ethereal() {
             </View>
         )
     }
-
     return (
-        <>
-            <View style={styles.etherealLogger}>
+        <View onLayout={handleLayout} style={{ flex: 1, position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}>
+            {/* <View style={styles.etherealLogger}>
                 {renderParam("Clock", clock.value)}
-            </View>
-            <Canvas
+            </View> */}
+            {activeLayer[0] && activeLayer[0].params && <Canvas
                 style={{ flex: 1, position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}
-                // onTouch={touchHandler}
+            // onTouch={touchHandler}
             >
                 <Group
                     transform={[
@@ -97,16 +96,17 @@ function Ethereal() {
                     ]}
                 >
                     <Path path={path} />
-                    
+
                     <SweepGradient
                         c={vec(0, 0)}
                         colors={['cyan', 'magenta', 'yellow', 'cyan']}
                     />
                     {/* <BlurMask blur={5} style="solid" /> */}
                 </Group>
-            </Canvas>
-        </>
+            </Canvas>}
+        </View>
     );
+
 }
 
 export default Ethereal;
